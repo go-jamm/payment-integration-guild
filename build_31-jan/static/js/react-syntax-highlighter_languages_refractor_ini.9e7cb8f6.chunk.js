41a3c512265151163704140611eea841f5a31030
mozammel-newroz <mozammel@fastsolutioninc.com>
2022-01-31 18:26:28 +0600

    remvoe deshipay

---
+++ build_31-jan/static/js/react-syntax-highlighter_languages_refractor_ini.9e7cb8f6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpayment_integration_guild=self.webpackChunkpayment_integration_guild||[]).push([[6495],{3409:function(t){function n(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=n,n.displayName="ini",n.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.9e7cb8f6.chunk.js.map
---
+++ build_31-jan/static/js/react-syntax-highlighter_languages_refractor_properties.000fbd95.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpayment_integration_guild=self.webpackChunkpayment_integration_guild||[]).push([[81],{9164:function(t){function n(t){t.languages.properties={comment:/^[ \t]*[#!].*$/m,"attr-value":{pattern:/(^[ \t]*(?:\\(?:\r\n|[\s\S])|[^\\\s:=])+?(?: *[=:] *| ))(?:\\(?:\r\n|[\s\S])|[^\\\r\n])+/m,lookbehind:!0},"attr-name":/^[ \t]*(?:\\(?:\r\n|[\s\S])|[^\\\s:=])+?(?= *[=:] *| )/m,punctuation:/[=:]/}}t.exports=n,n.displayName="properties",n.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_properties.000fbd95.chunk.js.map
